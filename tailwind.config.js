ad54f054ad47f12002dfc1f5a61a6d0ec1d6b253
Reihane Yaghoobzade <reihanygh2002@gmail.com>
2022-01-23 16:09:51 +0330

    make cintainer size settings and button links

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,44 @@ module.exports = {
                 brand: '#72569D',
             }
         },
+        container: {
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
+        },
     },
     variants: {
         extend: {
             ringWidth: ['hover', 'active'],
         },
     },
+    corePlugins: {
+        container: false
+    },
     plugins: [
         require('tailwindcss-rtl'),
+        function({ addComponents }) {
+            addComponents({
+                '.container': {
+                    maxWidth: '100%',
+                    '@screen sm': {
+                        maxWidth: '56rem',
+                    },
+                    '@screen md': {
+                        maxWidth: '64rem',
+                    },
+                    '@screen lg': {
+                        maxWidth: '72rem',
+                    },
+                    '@screen xl': {
+                        maxWidth: '80rem',
+                    },
+                }
+            })
+        },
     ],
 }
